4efc23fc40f649419a076e582986d7e956596884
kristoffer <kristoffersolsen@gmail.com>
2026-02-06 15:13:55 +0100

    implemented transactions

--- src/main/java/org/example/Sale.java
+++ src/main/java/org/example/Sale.java
@@ -1,4 +1,40 @@
 package org.example;
 
-public class Sale {
+/**
+ * A sale transaction
+ */
+public class Sale extends Transaction{
+
+    /**
+     *
+     * @param share The share to transact
+     * @param week Week of transaction
+     */
+    Sale(Share share, int week) {
+        super(share, week, new SaleCalculator(share));
+
+    }
+
+    /**
+     * Checks if transaction is valid and commits the transaction.
+     * @param player The player to perform a transaction on
+     */
+    @Override
+    public void commit(Player player) {
+        Portfolio portfolio = player.getportfolio();
+        // Check if player portfolio has the share
+        if (!portfolio.contains(share)) {
+            return;
+        }
+        // Check if transaction has been performed before
+        TransactionArchive archive = player.getTransactionArchive();
+        if (archive.contains(this)) {
+            return;
+        }
+        player.addMoney(this.calculator.calculateTotal());
+        player.getPortfolio().removeShare(share);
+        player.getTransactionArchive().add(this);
+
+        committed = true;
+    }
 }
--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -1,4 +1,48 @@
 package org.example;
 
-public class Transaction {
+/**
+ * Abstract class for all transactions
+ */
+public abstract class Transaction {
+    protected Share share;
+    protected int week;
+    protected TransactionCalculator calculator;
+    protected boolean committed = false;
+
+    /**
+     *
+     * @param share The share that is transacted
+     * @param week The week of the transaction
+     * @param calculator The calculator to use
+     */
+    Transaction(Share share, int week, TransactionCalculator calculator) {
+        this.share = share;
+        this.week = week;
+        this.calculator = calculator;
+    }
+
+
+    public Share getShare() {
+        return share;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public TransactionCalculator getCalculator() {
+        return calculator;
+    }
+
+    public boolean isCommitted() {
+        return committed;
+    }
+
+    /**
+     * Checks that transaction is valid and commits it
+     * @param player The player to perform a transaction on
+     */
+    public void commit(Player player) {
+
+    }
 }
