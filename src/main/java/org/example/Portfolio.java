98afcab3e5568698e6540fde5358421915a309d1
Kasper <kaspere@stud.ntnu.no>
2026-02-06 15:12:21 +0100

    Første utkast av Portfolio-klassen

--- src/main/java/org/example/Portfolio.java
+++ src/main/java/org/example/Portfolio.java
@@ -1,4 +1,54 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Portfolio {
+
+  private ArrayList<Share> shares;
+
+  Portfolio() {
+    shares = new ArrayList<>();
+  }
+
+  boolean addShare(Share share) {
+    if (!this.shares.contains(share)) {
+      this.shares.add(share);
+      return true;
+    }
+    return false;
+  }
+
+  boolean removeShare(Share share) {
+    if (this.shares.contains(share)) {
+      this.shares.remove(share);
+      return true;
+    }
+    return false;
+  }
+
+  ArrayList<Share> getShares() {
+    return this.shares;
+  }
+
+  ArrayList<Share> getShares(String symbol) {
+    ArrayList<Share> returnList = new ArrayList<>();
+
+    for (Share share : shares) {
+      if (share.getStock().getCompany().equals(symbol)) {
+        returnList.add(share);
+      }
+    }
+
+    return returnList;
+  }
+
+  boolean contains(Share shareToCheckFor) {
+    for (Share share : shares) {
+      if (share.getStock().equals(shareToCheckFor.getStock())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
