ed85fbdc1d4c39c95160c016017de21b3f5b90b5
kristoffer <kristoffersolsen@gmail.com>
2026-02-06 13:24:16 +0100

    initiate branch

--- src/main/java/org/example/PurchaseCalculator.java
+++ src/main/java/org/example/PurchaseCalculator.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class PurchaseCalculator implements TransactionCalculator{
+    BigDecimal calculateGross(){
+
+    }
+    BigDecimal calculateCommision(){
+
+    }
+    BigDecimal calculateTax(){
+
+    }
+    BigDecimal calculateTotal() {
+
+    }
 }
--- src/main/java/org/example/SaleCalculator.java
+++ src/main/java/org/example/SaleCalculator.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class SaleCalculator implements TransactionCalculator{
+
+
+    BigDecimal calculateGross(){
+
+    }
+    BigDecimal calculateCommision(){
+
+    }
+    BigDecimal calculateTax(){
+
+    }
+    BigDecimal calculateTotal() {
+
+    }
 }
